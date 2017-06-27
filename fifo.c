4428b733987b4610532c4c6240d27835cae78862
akunwar <aradhya.kunwar@mail.utoronto.ca>
2017-06-27 15:35:44 -0400

    fifo algoritm has been coded but yet to be tested

--- fifo.c
+++ fifo.c
@@ -12,13 +12,24 @@ extern int debug;
 
 extern struct frame *coremap;
 
+//data that will store the frame number in FIFO order
+struct QueueNode{
+	int frame;
+	QueueNode *next;
+};
+//pointer that stores the frame number that was accessed first
+struct QueueNode *first;
+//pointer that stores the frame number that was accessed last
+struct QueueNode *last;
+
 /* Page to evict is chosen using the fifo algorithm.
  * Returns the page frame number (which is also the index in the coremap)
  * for the page that is to be evicted.
  */
 int fifo_evict() {
-	
-	return 0;
+	int evictFrame = first->frame;
+	first = first->next;
+	return evictFrame;
 }
 
 /* This function is called on each access to a page to update any information
@@ -26,7 +37,10 @@ int fifo_evict() {
  * Input: The page table entry for the page that is being accessed.
  */
 void fifo_ref(pgtbl_entry_t *p) {
-
+	last->next = malloc(sizeof(struct QueueNode));
+	last = last->Next;
+	last->next = NULL
+	last->frame = p->frame;
 	return;
 }
 
@@ -34,4 +48,7 @@ void fifo_ref(pgtbl_entry_t *p) {
  * replacement algorithm 
  */
 void fifo_init() {
+	*first = malloc(sizeof(struct QueueNode));
+	first->next = NULL;
+	last = first;
 }
