e07f936e23badad2ea4ab46a646390998cb6682e
nathan78906 <nathan.seebarran@mail.utoronto.ca>
2017-06-29 09:28:32 -0400

    added fifo algo

--- fifo.c
+++ fifo.c
@@ -12,13 +12,42 @@ extern int debug;
 
 extern struct frame *coremap;
 
+//data that will store the frame number in FIFO order
+struct QueueNode{
+	unsigned int frame;
+	struct QueueNode *next;
+};
+//pointer that stores the frame number that was accessed first
+struct QueueNode *first;
+//pointer that stores the frame number that was accessed last
+struct QueueNode *last;
+
+int curr;
+
 /* Page to evict is chosen using the fifo algorithm.
  * Returns the page frame number (which is also the index in the coremap)
  * for the page that is to be evicted.
  */
 int fifo_evict() {
-	
-	return 0;
+	int evictFrame = first->frame;
+	evictFrame = curr; 
+	curr++;
+	//proper freeing mechanism for first QueueNode is still missing
+	if(first->next != NULL){
+		struct QueueNode *tempNode;
+		tempNode = first->next;
+		free(first);
+		first = tempNode;
+		//free(tempNode);
+	}
+
+	// if therse only one node in the Queue the pointer to the first node is not changed
+	/*
+	if(first->next != NULL){
+		first = first->next;
+	}
+	*/
+	return (evictFrame % memsize);
 }
 
 /* This function is called on each access to a page to update any information
@@ -26,12 +55,21 @@ int fifo_evict() {
  * Input: The page table entry for the page that is being accessed.
  */
 void fifo_ref(pgtbl_entry_t *p) {
-
-	return;
+	// the QueueNode for last has already been created
+	last->frame = p->frame;
+	// create QueueNode for future
+	last->next = malloc(sizeof(struct QueueNode));
+	last = last->next;
+	last->next = NULL;
 }
 
 /* Initialize any data structures needed for this 
  * replacement algorithm 
  */
 void fifo_init() {
+	first = malloc(sizeof(struct QueueNode));
+	first->next = NULL;
+	//since there's only QueueNode first and last QueueNodes are the same
+	last = first;
+	curr = 0;
 }
