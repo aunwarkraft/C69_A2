478ae5afc590deb505baf495bff31f7e045c1692
nathan78906 <nathan.seebarran@mail.utoronto.ca>
2017-06-29 11:29:53 -0400

    finished reading trace file and filling up address list

--- opt.c
+++ opt.c
@@ -5,12 +5,23 @@
 #include <stdlib.h>
 #include "pagetable.h"
 
+// from sim.h
+#define MAXLINE 256
 
 extern int memsize;
+
+// get tracefile path
+extern char *tracefile;
 
 extern int debug;
 
 extern struct frame *coremap;
+
+int size;
+addr_t *addr_list;
+
+void count_lines(FILE *infp);
+void populate_addr_list(FILE *infp);
 
 /* Page to evict is chosen using the optimal (aka MIN) algorithm. 
  * Returns the page frame number (which is also the index in the coremap)
@@ -34,6 +45,61 @@ void opt_ref(pgtbl_entry_t *p) {
  * replacement algorithm.
  */
 void opt_init() {
+    size = 0;
+    FILE *f;
+    f = fopen(tracefile, "r");
 
+    if (f == NULL) {
+        fprintf(stderr, "fopen: '%s': permision denied\n", tracefile);
+        exit(-1);
+    }
+    
+    count_lines(f);
+    // a list of addresses
+    addr_list = malloc(size * sizeof(addr_t));
+    
+    populate_addr_list(f);
+}
+
+// modified from sim.c
+void count_lines(FILE *infp) {
+	char buf[MAXLINE];
+	addr_t vaddr = 0;
+	char type;
+
+	while(fgets(buf, MAXLINE, infp) != NULL) {
+		if(buf[0] != '=') {
+			sscanf(buf, "%c %lx", &type, &vaddr);
+			if(debug)  {
+				printf("%c %lx\n", type, vaddr);
+			}
+			size++;
+		} else {
+			continue;
+		}
+
+	}
+}
+
+// modified from sim.c
+void populate_addr_list(FILE *infp) {
+	char buf[MAXLINE];
+	addr_t vaddr = 0;
+	char type;
+    int i = 0;
+
+	while(fgets(buf, MAXLINE, infp) != NULL) {
+		if(buf[0] != '=') {
+			sscanf(buf, "%c %lx", &type, &vaddr);
+			if(debug)  {
+				printf("%c %lx\n", type, vaddr);
+			}
+            addr_list[i] = vaddr;
+			i++;
+		} else {
+			continue;
+		}
+
+	}
 }
 
